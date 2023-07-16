8ab545134ba6a57b3532df25890850bbe19fed9c
Dr2546 <62285718+Dr2546@users.noreply.github.com>
2023-07-16 17:07:16 +0700

    Add files via upload

--- launch/controller_launch.py
+++ launch/controller_launch.py
@@ -2,26 +2,56 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+
+
+import xacro
+import yaml
 
 
 def generate_launch_description():
 
-    joy_node = Node(package='joy', executable='joy_node')
+   #Joy_connector node
+   joy_node = Node(package='joy', executable='joy_node')
 
-    joy_converter_node = Node(package='rpi_robot', executable='rpi_joy')
-    
-    #cam_node = Node(package='rpi_robot', executable='controller_cam')
-    cam_node = Node(package='rqt_image_view', executable='rqt_image_view')
+   #Joy_pub
+   joy_converter_node = Node(package='rpi_robot', executable='rpi_joy')
+   
+   #Camera_sub node
+   #cam_node = Node(package='rpi_robot', executable='controller_cam')
+   cam_node = Node(package='rqt_image_view', executable='rqt_image_view')
 
-    # Launch them all!
-    return LaunchDescription([
-       joy_node,
-       joy_converter_node,
-       cam_node
-    ])
+   #Getting robot config
+   rviz_config_dir = os.path.join(
+        get_package_share_directory('rpi_robot_description'),
+        'config',
+        'view_bot.rviz'
+    )
+
+   #Rviz node
+   display_rviz = Node(package='rviz2', executable='rviz2',
+                        name='rviz2',
+                        arguments=['-d', rviz_config_dir],
+                        output='screen')
+   
+   #SLAM
+   slam_node = IncludeLaunchDescription(
+               PythonLaunchDescriptionSource([
+                  FindPackageShare("slam_toolbox"), '/launch', '/online_async_launch.py']),
+                  launch_arguments={'slam_params_file':'~/ros2_ws/src/rpi_robot_description/config/mapper_params_online_async.yaml','use_sim_time':'false'}.items()
+         )
+
+   # Launch them all!
+   return LaunchDescription([
+      joy_node,
+      joy_converter_node,
+      cam_node,
+      display_rviz,
+      #slam_node
+   ])
--- launch/robot_launch.py
+++ launch/robot_launch.py
@@ -2,23 +2,60 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+
+
+import xacro
+import yaml
 
 
 def generate_launch_description():
 
-    motor_node = Node(package='rpi_robot', executable='rpi_motor')
+   #Joy_sub to motor_control node
+   motor_node = Node(package='rpi_robot', executable='rpi_motor')
 
-    cam_node = Node(package='rpi_robot', executable='rpi_cam')
-    
+   #Camera_pub node
+   cam_node = Node(package='rpi_robot', executable='rpi_cam')
 
-    # Launch them all!
-    return LaunchDescription([
-       motor_node,
-       cam_node
-    ])
+   #
+   rpi_robot_description_path = os.path.join(
+      get_package_share_directory('rpi_robot_description'))
+
+   #Getting robot urdf
+   xacro_file = os.path.join(rpi_robot_description_path,
+                           'urdf',
+                           'rpi_robot.urdf.xacro')
+
+   doc = xacro.parse(open(xacro_file))
+   xacro.process_doc(doc)
+   robot_description_config = doc.toxml()
+   robot_description = {'robot_description': robot_description_config}
+
+   #Robot_state_publisher node
+   node_robot_state_publisher = Node(
+      package='robot_state_publisher',
+      executable='robot_state_publisher',
+      output='screen',
+      parameters=[robot_description]
+   )
+
+   #Lidar_pub node
+   lidar_driver_node = IncludeLaunchDescription(
+               PythonLaunchDescriptionSource([
+                  FindPackageShare("ydlidar_ros2_driver"), '/launch', '/ydlidar_launch.py'])
+         )
+   
+
+   # Launch them all!
+   return LaunchDescription([
+      motor_node,
+      cam_node,
+      node_robot_state_publisher,
+      #lidar_driver_node
+   ])
